493e0ab10d5663b2f21a192f105d858bc0a1f928
Lívia Belizário <liviabelirocha@outlook.com>
2022-06-25 11:22:12 -0300

    feat: sorting

---
+++ Assets/Scripts/SpriteSortOrder.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSortOrder : MonoBehaviour
+{
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sortingOrder = Mathf.RoundToInt(transform.position.y * -10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
